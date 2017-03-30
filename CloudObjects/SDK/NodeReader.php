d55f165e79b8a861a3defdbe3fe9a0fb4239895e
Lukas Rosenstock <lr@lukasrosenstock.net>
2017-03-30 18:05:19 +0200

    Additional convenience functions in NodeReader

--- CloudObjects/SDK/NodeReader.php
+++ CloudObjects/SDK/NodeReader.php
@@ -7,6 +7,7 @@
 namespace CloudObjects\SDK;
 
 use ML\JsonLD\Node;
+use ML\IRI\IRI;
 
 /**
  * The NodeReader provides some convenience methods for reading information
@@ -55,6 +56,20 @@ class NodeReader {
             return true;
         else
             return false;
+        
+        return false;
+    }
+
+    private function getFirstValue(Node $node = null, $property, $default = null) {
+        if (!isset($node))
+            return $default;
+        $valueFromNode = $node->getProperty($this->expand($property));
+        if (!isset($valueFromNode))
+            return $default;
+        if (is_array($valueFromNode))
+            return $valueFromNode[0];
+        
+        return $valueFromNode;
     }
 
     /**
@@ -68,18 +83,56 @@ class NodeReader {
      * @return string|null
      */
     public function getFirstValueString(Node $node = null, $property, $default = null) {
-        if (!isset($node))
+        $valueFromNode = $this->getFirstValue($node, $property, $default);
+        if ($valueFromNode == $default)
             return $default;
-        $valueFromNode = $node->getProperty($this->expand($property));
-        if (!isset($valueFromNode))
-            return $default;
-        if (is_array($valueFromNode))
-            $valueFromNode = $valueFromNode[0];
         
         if (is_a($valueFromNode, 'ML\JsonLD\Node'))
             return $valueFromNode->getId();
         else
             return $valueFromNode->getValue();
+    }
+
+    /**
+     * Reads a property from a node and converts it into a IRI.
+     * If the property has multiple values only the first is returned.
+     * If no value is found, value is a literal or the node is null, the default is returned.
+     *
+     * @param Node $node The node to work on.
+     * @param string|object $property The property to read.
+     * @param $default The default that is returned if no value for the property exists on the node.
+     * @return string|null
+     */
+    public function getFirstValueIRI(Node $node = null, $property, IRI $default = null) {
+        $valueFromNode = $this->getFirstValue($node, $property, $default);
+        if ($valueFromNode == $default)
+            return $default;
+        
+        if (is_a($valueFromNode, 'ML\JsonLD\Node'))
+            return new IRI($valueFromNode->getId());
+        else
+            return $default;
+    }
+
+    /**
+     * Reads a property from a node and returns it as a Node.
+     * If the property has multiple values only the first is returned.
+     * If no value is found, value is a literal or the node is null, the default is returned.
+     *
+     * @param Node $node The node to work on.
+     * @param string|object $property The property to read.
+     * @param $default The default that is returned if no value for the property exists on the node.
+     * @return string|null
+     */
+    public function getFirstValueNode(Node $node = null, $property, Node $default = null) {
+        $valueFromNode = $this->getFirstValue($node, $property, $default);
+        if ($valueFromNode == $default)
+            return $default;
+        
+        if (is_a($valueFromNode, 'ML\JsonLD\Node'))
+            return $valueFromNode;
+        else
+            return $default;
     }
 
     /**
@@ -111,4 +164,72 @@ class NodeReader {
         
         return false;
     }
+
+    private function getAllValues(Node $node = null, $property) {
+        if (!isset($node))
+            return [];
+
+        $valueFromNode = $node->getProperty($this->expand($property));
+        if (!isset($valueFromNode))
+            return [];
+        if (!is_array($valueFromNode))
+            $valueFromNode = [$valueFromNode];
+        return $valueFromNode;
+    }
+
+    /**
+     * Reads all values from a node and returns them as a string array.
+     *
+     * @param Node $node The node to work on.
+     * @param string|object $property The property to read.
+     * @return array<string>
+     */
+    public function getAllValuesString(Node $node = null, $property) {
+        $allValues = $this->getAllValues($node, $property);
+        $output = [];
+        foreach ($allValues as $a)
+            if (is_a($a, 'ML\JsonLD\Node'))
+                $output[] = $a->getId();
+            else
+                $output[] = $a->getValue();
+
+        return $output;
+    }
+
+    /**
+     * Reads all values from a node and returns them as a IRI array.
+     * Only converts the Node IDs of nodes into IRI, literal values are skipped.
+     *
+     * @param Node $node The node to work on.
+     * @param string|object $property The property to read.
+     * @return array<IRI>
+     */
+    public function getAllValuesIRI(Node $node = null, $property) {
+        $allValues = $this->getAllValues($node, $property);
+        $output = [];
+        foreach ($allValues as $a)
+            if (is_a($a, 'ML\JsonLD\Node'))
+                $output[] = new IRI($a->getId());
+
+        return $output;
+    }
+
+    /**
+     * Reads all values from a node and returns them as a Node array.
+     * Returns only nodes, literal values are skipped.
+     *
+     * @param Node $node The node to work on.
+     * @param string|object $property The property to read.
+     * @return array<Node>
+     */
+    public function getAllValuesNode(Node $node = null, $property) {
+        $allValues = $this->getAllValues($node, $property);
+        $output = [];
+        foreach ($allValues as $a)
+            if (is_a($a, 'ML\JsonLD\Node'))
+                $output[] = $a;
+
+        return $output;
+    }
+
 }
--- tests/OfflineTests/NodeReaderMockTest.php
+++ tests/OfflineTests/NodeReaderMockTest.php
@@ -24,7 +24,7 @@ class NodeReaderMockTest extends \PHPUnit_Framework_TestCase {
   private function useRootResourceMock() {
     $this->setMockResponse(new Response(200,
       ['Content-Type' => 'application/ld+json'],
-      '{"@context":{"cloudobjects":"coid:\/\/cloudobjects.io\/","rdf":"http:\/\/www.w3.org\/1999\/02\/22-rdf-syntax-ns#","rdfs":"http:\/\/www.w3.org\/2000\/01\/rdf-schema#"},"@id":"coid:\/\/cloudobjects.io","@type":"cloudobjects:Namespace","cloudobjects:hasPublicListing":"true","cloudobjects:revision":"1-325baa62b76105f56dc09386f5a2ec91","rdfs:comment":"The CloudObjects namespace defines the essential objects.","rdfs:label":"CloudObjects"}'));
+      '{"@context":{"co":"coid:\/\/cloudobjects.io\/","rdf":"http:\/\/www.w3.org\/1999\/02\/22-rdf-syntax-ns#","agws":"coid:\/\/accountgateways.cloudobjects.io\/","rdfs":"http:\/\/www.w3.org\/2000\/01\/rdf-schema#"},"@id":"coid:\/\/cloudobjects.io","@type":["agws:Service","co:Namespace"],"co:isAtRevision":"6-fbea0c90b2c5e5300e4039ed99be9b2d","co:isVisibleTo":{"@id":"co:Public"},"co:recommendsPrefix":"co","co:wasUpdatedAt":{"@type":"http:\/\/www.w3.org\/2001\/XMLSchema#dateTime","@value":"2017-01-16T17:29:22+00:00"},"rdfs:comment":"The CloudObjects namespace defines the essential objects.","rdfs:label":"CloudObjects"}'));
   }
 
   protected function setUp() {
@@ -70,6 +70,87 @@ class NodeReaderMockTest extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals('theDefaultValue', $this->reader->getFirstValueString($object, 'coid://cloudobjects.io/makesTriplesVisibleTo', 'theDefaultValue'));
     $this->assertEquals('theDefaultValue', $this->reader->getFirstValueString($object, 'co:makesTriplesVisibleTo', 'theDefaultValue'));
+  }
+
+  public function testGetFirstValueIRI1() {
+    $coid = new IRI('coid://cloudobjects.io');
+    $this->useRootResourceMock();
+    $object = $this->retriever->getObject($coid);
+
+    $this->assertInstanceOf('ML\IRI\IRI', $this->reader->getFirstValueIRI($object, 'coid://cloudobjects.io/isVisibleTo'));
+    $this->assertInstanceOf('ML\IRI\IRI', $this->reader->getFirstValueIRI($object, 'co:isVisibleTo'));
+    
+    $this->assertEquals(new IRI('coid://cloudobjects.io/Public'), $this->reader->getFirstValueIRI($object, 'coid://cloudobjects.io/isVisibleTo'));
+    $this->assertEquals(new IRI('coid://cloudobjects.io/Public'), $this->reader->getFirstValueIRI($object, 'co:isVisibleTo'));
+  }
+
+  public function testGetFirstValueNode1() {
+    $coid = new IRI('coid://cloudobjects.io');
+    $this->useRootResourceMock();
+    $object = $this->retriever->getObject($coid);
+
+    $this->assertInstanceOf('ML\JsonLD\Node', $this->reader->getFirstValueNode($object, 'coid://cloudobjects.io/isVisibleTo'));
+    $this->assertInstanceOf('ML\JsonLD\Node', $this->reader->getFirstValueNode($object, 'co:isVisibleTo'));
+    
+    $this->assertEquals('coid://cloudobjects.io/Public', $this->reader->getFirstValueNode($object, 'coid://cloudobjects.io/isVisibleTo')->getId());
+    $this->assertEquals('coid://cloudobjects.io/Public', $this->reader->getFirstValueNode($object, 'co:isVisibleTo')->getId());
+  }
+
+  public function testGetAllValuesString1() {
+    $coid = new IRI('coid://cloudobjects.io');
+    $this->useRootResourceMock();
+    $object = $this->retriever->getObject($coid);
+    
+    $this->assertCount(1, $this->reader->getAllValuesString($object, 'http://www.w3.org/2000/01/rdf-schema#label'));
+    $this->assertCount(1, $this->reader->getAllValuesString($object, 'rdfs:label'));
+
+    $this->assertEquals('CloudObjects', $this->reader->getAllValuesString($object, 'http://www.w3.org/2000/01/rdf-schema#label')[0]);
+    $this->assertEquals('CloudObjects', $this->reader->getAllValuesString($object, 'rdfs:label')[0]);
+
+    $this->assertCount(0, $this->reader->getAllValuesString($object, 'coid://cloudobjects.io/makesTriplesVisibleTo'));
+    $this->assertCount(0, $this->reader->getAllValuesString($object, 'co:makesTriplesVisibleTo'));
+
+    $this->assertCount(2, $this->reader->getAllValuesString($object, '@type'));
+  }
+
+  public function testGetAllValuesIRI1() {
+    $coid = new IRI('coid://cloudobjects.io');
+    $this->useRootResourceMock();
+    $object = $this->retriever->getObject($coid);
+
+    $this->assertCount(0, $this->reader->getAllValuesIRI($object, 'http://www.w3.org/2000/01/rdf-schema#label'));
+    $this->assertCount(0, $this->reader->getAllValuesIRI($object, 'rdfs:label'));
+
+    $this->assertCount(1, $this->reader->getAllValuesIRI($object, 'coid://cloudobjects.io/isVisibleTo'));
+    $this->assertCount(1, $this->reader->getAllValuesIRI($object, 'co:isVisibleTo'));
+
+    $this->assertCount(2, $this->reader->getAllValuesIRI($object, '@type'));
+
+    $this->assertInstanceOf('ML\IRI\IRI', $this->reader->getAllValuesIRI($object, 'coid://cloudobjects.io/isVisibleTo')[0]);
+    $this->assertInstanceOf('ML\IRI\IRI', $this->reader->getAllValuesIRI($object, 'co:isVisibleTo')[0]);
+
+    $this->assertEquals(new IRI('coid://cloudobjects.io/Public'), $this->reader->getAllValuesIRI($object, 'coid://cloudobjects.io/isVisibleTo')[0]);
+    $this->assertEquals(new IRI('coid://cloudobjects.io/Public'), $this->reader->getAllValuesIRI($object, 'co:isVisibleTo')[0]);
+  }
+
+  public function testGetAllValuesNode1() {
+    $coid = new IRI('coid://cloudobjects.io');
+    $this->useRootResourceMock();
+    $object = $this->retriever->getObject($coid);
+
+    $this->assertCount(0, $this->reader->getAllValuesNode($object, 'http://www.w3.org/2000/01/rdf-schema#label'));
+    $this->assertCount(0, $this->reader->getAllValuesNode($object, 'rdfs:label'));
+
+    $this->assertCount(1, $this->reader->getAllValuesNode($object, 'coid://cloudobjects.io/isVisibleTo'));
+    $this->assertCount(1, $this->reader->getAllValuesNode($object, 'co:isVisibleTo'));
+
+    $this->assertCount(2, $this->reader->getAllValuesNode($object, '@type'));
+
+    $this->assertInstanceOf('ML\JsonLD\Node', $this->reader->getAllValuesNode($object, 'coid://cloudobjects.io/isVisibleTo')[0]);
+    $this->assertInstanceOf('ML\JsonLD\Node', $this->reader->getAllValuesNode($object, 'co:isVisibleTo')[0]);
+
+    $this->assertEquals('coid://cloudobjects.io/Public', $this->reader->getAllValuesNode($object, 'coid://cloudobjects.io/isVisibleTo')[0]->getId());
+    $this->assertEquals('coid://cloudobjects.io/Public', $this->reader->getAllValuesNode($object, 'co:isVisibleTo')[0]->getId());
   }
 
 }
