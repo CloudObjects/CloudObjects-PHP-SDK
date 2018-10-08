d7c06fbc8112699b9dad88249b6bea88619a590e
Lukas Rosenstock <lr@lukasrosenstock.net>
2018-10-08 22:12:23 +0200

    Added support for oauth2:FixedBearerTokenAuthentication

---
+++ CloudObjects/SDK/Exceptions/InvalidObjectConfigurationException.php
@@ -0,0 +1,15 @@
+<?php
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
+namespace CloudObjects\SDK\Exceptions;
+
+/**
+ * An Exception that is thrown when an object's configuration
+ * doesn't match the client's expectations.
+ */
+class InvalidObjectConfigurationException extends \Exception {
+
+}
--- CloudObjects/SDK/WebAPI/APIClientFactory.php
+++ CloudObjects/SDK/WebAPI/APIClientFactory.php
@@ -9,12 +9,30 @@ namespace CloudObjects\SDK\WebAPI;
 use ML\JsonLD\Node;
 use CloudObjects\SDK\NodeReader;
 use GuzzleHttp\Client;
+use CloudObjects\SDK\Exceptions\InvalidObjectConfigurationException;
 
 /**
  * The APIClientFactory can be used to create a preconfigured Guzzle HTTP API client
  * based on the configuration data available for an API on CloudObjects.
  */
 class APIClientFactory {
+
+    private static function configureBearerTokenAuthentication(Node $api, Node $namespace, NodeReader $reader, array $clientConfig) {
+        $accessToken = $reader->getFirstValueString($api, 'oauth2:hasFixedBearerToken');
+
+        if (!isset($accessToken)) {
+            $tokenProperty = $reader->getFirstValueString($api, 'oauth2:usesFixedBearerTokenFrom');
+            if (!isset($tokenProperty))
+                throw new InvalidObjectConfigurationException("An API must have either a fixed access token or a defined token property.");
+            $accessToken = $reader->getFirstValueString($namespace, $tokenProperty);
+            if (!isset($accessToken))
+                throw new InvalidObjectConfigurationException("The namespace does not have a value for <".$tokenProperty.">.");
+        }
+
+        $clientConfig['headers']['Authorization'] = 'Bearer ' . $accessToken;
+
+        return $clientConfig;
+    }
 
     private static function configureBasicAuthentication(Node $api, Node $namespace, NodeReader $reader, array $clientConfig) {
         $username = $reader->getFirstValueString($api, 'wa:hasFixedUsername');
@@ -23,19 +41,19 @@ class APIClientFactory {
         if (!isset($username)) {
             $usernameProperty = $reader->getFirstValueString($api, 'wa:usesUsernameFrom');
             if (!isset($usernameProperty))
-                throw new \Exception("An API must have either a fixed username or a defined username property.");
+                throw new InvalidObjectConfigurationException("An API must have either a fixed username or a defined username property.");
             $username = $reader->getFirstValueString($namespace, $usernameProperty);
             if (!isset($username))
-                throw new \Exception("The namespace does not have a value for <".$usernameProperty.">.");
+                throw new InvalidObjectConfigurationException("The namespace does not have a value for <".$usernameProperty.">.");
         }
 
         if (!isset($password)) {
             $passwordProperty = $reader->getFirstValueString($api, 'wa:usesPasswordFrom');
             if (!isset($passwordProperty))
-                throw new \Exception("An API must have either a fixed password or a defined password property.");
+                throw new InvalidObjectConfigurationException("An API must have either a fixed password or a defined password property.");
             $password = $reader->getFirstValueString($namespace, $passwordProperty);
             if (!isset($password))
-                throw new \Exception("The namespace does not have a value for <".$passwordProperty.">.");
+                throw new InvalidObjectConfigurationException("The namespace does not have a value for <".$passwordProperty.">.");
         }
         
         $clientConfig['auth'] = [$username, $password];
@@ -50,19 +68,24 @@ class APIClientFactory {
     public static function createClient(Node $api, Node $namespace) {
         $reader = new NodeReader([
             'prefixes' => [
-                'wa' => 'coid://webapi.cloudobjects.io/'
+                'wa' => 'coid://webapi.cloudobjects.io/',
+                'oauth2' => 'coid://oauth2.cloudobjects.io/'
             ]
         ]);
 
         if (!$reader->hasType($api, 'wa:WebAPI'))
-            throw new \Exception("The API node must have the type <coid://webapi.cloudobjects.io/WebAPI>.");
+            throw new InvalidObjectConfigurationException("The API node must have the type <coid://webapi.cloudobjects.io/WebAPI>.");
         
         $baseUrl = $reader->getFirstValueString($api, 'wa:hasBaseURL');
         if (!isset($baseUrl))
-            throw new \Exception("The API must have a base URL.");
+            throw new InvalidObjectConfigurationException("The API must have a base URL.");
         
         $clientConfig = [ 'base_uri' => $baseUrl ];
 
+        if ($reader->hasPropertyValue($api, 'wa:supportsAuthenticationMechanism',
+                'oauth2:FixedBearerTokenAuthentication'))
+            $clientConfig = self::configureBearerTokenAuthentication($api, $namespace, $reader, $clientConfig);
+        else
         if ($reader->hasPropertyValue($api, 'wa:supportsAuthenticationMechanism',
                 'wa:HTTPBasicAuthentication'))
             $clientConfig = self::configureBasicAuthentication($api, $namespace, $reader, $clientConfig);
