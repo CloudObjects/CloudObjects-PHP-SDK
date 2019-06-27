2db314ef0e1236bcbb087da994c3f91260bdfb64
Lukas Rosenstock <lr@lukasrosenstock.net>
2019-06-27 16:59:33 +0200

    Refactor SharedSecretAuthentication to non-static

--- CloudObjects/SDK/Helpers/SharedSecretAuthentication.php
+++ CloudObjects/SDK/Helpers/SharedSecretAuthentication.php
@@ -22,8 +22,18 @@ class SharedSecretAuthentication {
     const RESULT_SHARED_SECRET_NOT_RETRIEVABLE = 4;
     const RESULT_SHARED_SECRET_INCORRECT = 5;
 
+    private $objectRetriever;
+
+    /**
+     * @param ObjectRetriever $objectRetriever An initialized and authenticated object retriever.
+     */
+    public function __construct(ObjectRetriever $objectRetriever) {
+        $this->objectRetriever = $objectRetriever;
+    }
+
     /**
      * Verifies credentials.
+     * @deprecated
      * 
      * @param ObjectRetriever $retriever Provides access to CloudObjects.
      * @param string $username Username; a domain.
@@ -41,6 +51,43 @@ class SharedSecretAuthentication {
 
         // Retrieve namespace
         $namespace = $retriever->getObject($namespaceCoid);
+        if (!isset($namespace))
+            return self::RESULT_NAMESPACE_NOT_FOUND;
+
+        // Read and validate shared secret
+        $reader = new NodeReader([
+            'prefixes' => [
+                'co' => 'coid://cloudobjects.io/'
+            ]
+        ]);
+        $sharedSecret = $reader->getAllValuesNode($namespace, 'co:hasSharedSecret');
+        if (count($sharedSecret) != 1)
+            return self::RESULT_SHARED_SECRET_NOT_RETRIEVABLE;
+        
+        if ($reader->getFirstValueString($sharedSecret[0], 'co:hasTokenValue') == $password)
+            return self::RESULT_OK;
+        else
+            return self::RESULT_SHARED_SECRET_INCORRECT;        
+    }
+
+    /**
+     * Verifies credentials.
+     *
+     * @param string $username Username; a domain.
+     * @param string $password Password; a shared secret.
+     * 
+     * @return integer A result constant, RESULT_OK if successful.
+     */
+    public function verify($username, $password) {
+        // Validate input
+        $namespaceCoid = new IRI('coid://'.$username);
+        if (COIDParser::getType($namespaceCoid) != COIDParser::COID_ROOT)
+            return self::RESULT_INVALID_USERNAME;
+        if (strlen($password) != 40)
+            return self::RESULT_INVALID_PASSWORD;
+
+        // Retrieve namespace
+        $namespace = $this->objectRetriever->getObject($namespaceCoid);
         if (!isset($namespace))
             return self::RESULT_NAMESPACE_NOT_FOUND;
 
